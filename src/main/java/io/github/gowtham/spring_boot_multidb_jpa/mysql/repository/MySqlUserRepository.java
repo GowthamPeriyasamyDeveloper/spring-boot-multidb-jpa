323d246e0fa8918c9f8bba4e490438e02316029d
-- <gowthamperiyasamy04@gmail.com>
2026-02-04 10:38:08 +0530

    Revert "initial commit"
    This reverts commit 3ce84d9a395df844a0edfb3420d352dc0b693fa8.

--- src/main/java/io/github/gowtham/spring_boot_multidb_jpa/mysql/repository/MySqlUserRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.gowtham.spring_boot_multidb_jpa.mysql.repository;
-
-import io.github.gowtham.spring_boot_multidb_jpa.mysql.entity.MySqlUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MySqlUserRepository extends JpaRepository<MySqlUser, Long>
-{
-
-}
--- src/main/java/io/github/gowtham/spring_boot_multidb_jpa/postgres/repository/PostgresEmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.gowtham.spring_boot_multidb_jpa.postgres.repository;
-
-import io.github.gowtham.spring_boot_multidb_jpa.postgres.entity.PostgresEmployee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostgresEmployeeRepository extends JpaRepository<PostgresEmployee, Long>
-{
-}
